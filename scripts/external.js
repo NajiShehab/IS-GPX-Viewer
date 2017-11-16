75437e658f3adf464bae91e1cc5f8fe1eb3295b7
Salman <2194437M@student.gla.ac.uk@>
2017-11-16 15:43:18 +0000

    Closes #1

--- scripts/external.js
+++ scripts/external.js
@@ -1,5 +1,7 @@
-$(document).ready(function(){
-    $("p").click(function(){
-        $(this).hide();
-    });
-});
+var map;
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+	  center: {lat: 55.855, lng: -4.242},
+	  zoom: 8
+	});
+}
